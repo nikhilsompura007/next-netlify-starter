1f4a4c127c65220ff84fe3b3e3fd8879e3989091
nikhilsompura007 <nikhilsompura007@gmail.com>
2023-11-26 12:57:58 +0530

    changes in NavBar Page

--- pages/NavBar/index.jsx
+++ pages/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function NavBar() {
+export default function NavBar() {
   return (
     <div className='NavBarDiv'>
     <a className='NavBarAnc' href='/'>Home</a> <a className='NavBarAnc' href='/tools'>Tools</a>
