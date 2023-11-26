f9c962dd8d5e0a37dad54e8c7a7eee80c0e11fb5
nikhilsompura007 <nikhilsompura007@gmail.com>
2023-11-26 13:25:35 +0530

    Updated the Navbar location

--- pages/tools/index.jsx
+++ pages/tools/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { NavBar } from 'pages/NavBar'
+import NavBar from '@components/NavBar'
 import {xss} from './xss'
 
 
